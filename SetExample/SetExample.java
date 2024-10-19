4f364af9a5e42f45e2997031bfce0d5134ebb787
DilshanAbbey <118147921+DilshanAbbey@users.noreply.github.com>
2024-10-19 20:53:30 +0530

    Add files via upload

--- SetExample/SetExample.java
+++ SetExample/SetExample.java
@@ -1,1 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
 
+class Ex{
+	public Ex()
+	{
+		Set<Integer> set = new HashSet();
+		set.add(5);
+		set.add(4);
+		set.add(5);
+		set.add(3);
+		set.add(2);
+		set.remove(3);
+		System.out.println(set);
+	}
+	
+	public static void main(String[] args)
+	{
+		new Ex();
+	}
+}
