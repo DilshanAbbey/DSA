2bcd41ec3b1a62c5ddc788e2a1a2cde6f1335aff
DilshanAbbey <118147921+DilshanAbbey@users.noreply.github.com>
2024-10-23 12:37:49 +0530

    Add files via upload

--- Sequenced Collection Practical 1/SequencedCollectionpractical1.java
+++ Sequenced Collection Practical 1/SequencedCollectionpractical1.java
@@ -1,1 +1,25 @@
+import java.util.LinkedList;
+import java.util.SequencedCollection;
 
+class SequencedCollectionpractical1
+{
+	SequencedCollectionpractical1()
+	{
+		SequencedCollection sc = new LinkedList();
+		sc.addFirst(5);
+		sc.addFirst(2);
+		sc.addLast(3);
+		
+		System.out.println(sc);
+		System.out.println(sc.getFirst());
+		System.out.println(sc.removeLast());
+		System.out.println(sc);
+		System.out.println(sc.reversed());
+	
+	}
+		
+	public static void main(String[] args)
+	{
+		new SequencedCollectionpractical1();
+	}
+}
