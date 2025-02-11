52ec41ede63cd200eb9fcf487b0d70d174602bc6
DilshanAbbey <118147921+DilshanAbbey@users.noreply.github.com>
2025-02-11 08:15:31 +0530

    Add files via upload

--- RabbitFibonacci/RabbitFibonacci.java
+++ RabbitFibonacci/RabbitFibonacci.java
@@ -1,1 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rabbitfibonacci;
 
+/**
+ *
+ * @author ACER
+ */
+public class RabbitFibonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static int rabbitPairs (int months)
+    {
+        if(months == 0 || months == 1) //Base case - No of rabbits in month 0 or 1
+        {
+            return 1;
+        }
+        
+        return rabbitPairs(months - 1) + rabbitPairs(months - 2);
+        
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int months = 14;
+        
+        System.out.println("Number of rabbit pairs after " + months + " months is "+ rabbitPairs(months));
+        
+    }
+    
+}
