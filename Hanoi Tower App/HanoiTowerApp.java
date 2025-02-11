4abcab56d9154649b723362869be4f44eeffc479
DilshanAbbey <118147921+DilshanAbbey@users.noreply.github.com>
2025-02-11 10:11:39 +0530

    Add files via upload

--- Hanoi Tower App/HanoiTowerApp.java
+++ Hanoi Tower App/HanoiTowerApp.java
@@ -1,1 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hanoitowerapp;
 
+/**
+ *
+ * @author ACER
+ */
+public class HanoiTowerApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static int hanoiCount(int no_of_disks)
+    {
+        if(no_of_disks == 1)
+        {
+            return 1;
+        }
+        
+        return 2 * hanoiCount(no_of_disks - 1) + 1;
+        
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int noOfDisks = 5;
+        int no_of_steps;
+        no_of_steps = hanoiCount(noOfDisks);
+        System.out.println("Total number of steps : " + no_of_steps);
+    }
+    
+}
